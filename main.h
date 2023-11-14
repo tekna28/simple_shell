7021cbe1acc4a6ea7aa9706482e83c4b8daaa1aa
tekna28 <salma.jeghloul@gmail.com>
2023-11-14 09:41:03 -0800

    First commit

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef _HEADER_
+#define _HEADER_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
